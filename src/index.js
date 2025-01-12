78cb1cf598012df7d1724092222426a977c576e0
sahand mahdilu <sahand.mahdilu29@gmail.com>
2025-01-12 21:21:47 +0330

    data id updated

--- src/index.js
+++ src/index.js
@@ -1,21 +1,35 @@
 let productArray=[
     {id:1, title:"cx GAME", price:700, count:1, image:"../public/images/1.png"},
-    {id:1, title:"ASUS", price:600, count:1, image:"../public/images/2.png"},
-    {id:1, title:"DELL", price:500, count:1, image:"../public/images/3.png"},
-    {id:1, title:"zx 2000", price:400, count:1, image:"../public/images/4.png"},
-    {id:1, title:"INVIDIA", price:1000, count:1, image:"../public/images/5.png"},
-    {id:1, title:"HP", price:750, count:1, image:"../public/images/6.png"},
-    {id:1, title:"AMD GAME ", price:730, count:1, image:"../public/images/7.png"},
-    {id:1, title:"RIZEN", price:800, count:1, image:"../public/images/8.png"},
-    {id:1, title:"KX 1200", price:900, count:1, image:"../public/images/9.png"},
-    {id:1, title:"LENOVO GAME", price:950, count:1, image:"../public/images/10.png"},
-    {id:1, title:"LEGION", price:450, count:1, image:"../public/images/11.png"},
-    {id:1, title:"HX 200", price:720, count:1, image:"../public/images/12.png"},
-    {id:1, title:"ROG 1700", price:560, count:1, image:"../public/images/13.png"},
-    {id:1, title:"GFORCE x90", price:920, count:1, image:"../public/images/14.png"},
-    {id:1, title:"GCX 500", price:590, count:1, image:"../public/images/15.png"},
-    {id:1, title:"GLADIATOR 10X", price:800, count:1, image:"../public/images/16.png"},
+    {id:2, title:"ASUS", price:600, count:1, image:"../public/images/2.png"},
+    {id:3, title:"DELL", price:500, count:1, image:"../public/images/3.png"},
+    {id:4, title:"zx 2000", price:400, count:1, image:"../public/images/4.png"},
+    {id:5, title:"INVIDIA", price:1000, count:1, image:"../public/images/5.png"},
+    {id:6, title:"HP", price:750, count:1, image:"../public/images/6.png"},
+    {id:7, title:"AMD GAME ", price:730, count:1, image:"../public/images/7.png"},
+    {id:8, title:"RIZEN", price:800, count:1, image:"../public/images/8.png"},
+    {id:9, title:"KX 1200", price:900, count:1, image:"../public/images/9.png"},
+    {id:10, title:"LENOVO GAME", price:950, count:1, image:"../public/images/10.png"},
+    {id:11, title:"LEGION", price:450, count:1, image:"../public/images/11.png"},
+    {id:12, title:"HX 200", price:720, count:1, image:"../public/images/12.png"},
+    {id:13, title:"ROG 1700", price:560, count:1, image:"../public/images/13.png"},
+    {id:14, title:"GFORCE x90", price:920, count:1, image:"../public/images/14.png"},
+    {id:15, title:"GCX 500", price:590, count:1, image:"../public/images/15.png"},
+    {id:16, title:"GLADIATOR 10X", price:800, count:1, image:"../public/images/16.png"},
 ]
+
+const dataContainerElem = document.getElementById("container")
+console.log(dataContainerElem);
 
 
 console.log(productArray);
+
+let currentPage=1
+let dataCount = 6 
+
+function displayData(allData,currentPage,dataCount,dataContainer){
+
+    let startIndex= 
+
+
+
+}
