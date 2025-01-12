6346833cdc4970080ced44e8ec81179dfe27d968
sahand mahdilu <sahand.mahdilu29@gmail.com>
2025-01-12 21:09:26 +0330

    data added

---
+++ src/index.js
@@ -0,0 +1,18 @@
+let productArray=[
+    {id:1, title:"cx GAME", price:700, count:1, image:"../public/images/1.png"},
+    {id:1, title:"ASUS", price:600, count:1, image:"../public/images/2.png"},
+    {id:1, title:"DELL", price:500, count:1, image:"../public/images/3.png"},
+    {id:1, title:"zx 2000", price:400, count:1, image:"../public/images/4.png"},
+    {id:1, title:"INVIDIA", price:1000, count:1, image:"../public/images/5.png"},
+    {id:1, title:"HP", price:750, count:1, image:"../public/images/6.png"},
+    {id:1, title:"AMD GAME ", price:730, count:1, image:"../public/images/7.png"},
+    {id:1, title:"RIZEN", price:800, count:1, image:"../public/images/8.png"},
+    {id:1, title:"KX 1200", price:900, count:1, image:"../public/images/9.png"},
+    {id:1, title:"LENOVO GAME", price:950, count:1, image:"../public/images/10.png"},
+    {id:1, title:"LEGION", price:450, count:1, image:"../public/images/11.png"},
+    {id:1, title:"HX 200", price:720, count:1, image:"../public/images/12.png"},
+    {id:1, title:"ROG 1700", price:560, count:1, image:"../public/images/13.png"},
+    {id:1, title:"GFORCE x90", price:920, count:1, image:"../public/images/14.png"},
+    {id:1, title:"GCX 500", price:590, count:1, image:"../public/images/15.png"},
+    {id:1, title:"GLADIATOR 10X", price:800, count:1, image:"../public/images/16.png"},
+]
